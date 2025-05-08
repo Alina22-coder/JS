78eb4c2d586f54c65c128bd812ab97c679efae39
Alina_bkl <alinaburlakova06@gmail.com>
2025-05-08 22:19:07 +0300

    -created lesson 2

---
+++ Lesson 2/hw2-3/main.js
@@ -0,0 +1,31 @@
+// #LARqoUj5I
+let book1 = {
+    title: 'Я бачу, вас цікавить пітьма',
+    pageCount: 639,
+    genre: 'Фікшн',
+    authors: [
+        {name:'Ілларіон Павлюк', age:44}
+    ]
+}
+console.log(book1);
+
+let book2 = {
+    title: 'Залишся, якщо кохаєш',
+    pageCount: 288,
+    genre: 'Любовний роман',
+    authors: [
+        {name:'Коллін Гувер', age:45}
+    ]
+}
+console.log(book2);
+
+let book3 ={
+    title: 'Переломний рік. 365 днів, щоб стати людиною, якою ви справді хочете бути',
+    pageCount: 384,
+    genre: 'Книги по психології',
+    authors: [
+        {name:'Бріанна Вест', age:32}
+
+    ]
+}
+console.log(book3);
---
+++ Lesson 2/hw2-5/main.js
@@ -0,0 +1,11 @@
+// #coYydZuaeEB
+let weather=[
+    {morning:5, day:17, evening:9},
+    {morning:10, day:15, evening:10},
+    {morning:8, day:13, evening:7},
+    {morning:3, day:18, evening:12},
+    {morning:5, day:19, evening:10},
+    {morning:9, day:11, evening:8},
+    {morning:8, day:10, evening:7},
+]
+console.log(weather);
