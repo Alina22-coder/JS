385cc22175ab5da3d433f6db35b408bcc689811f
Alina_bkl <alinaburlakova06@gmail.com>
2025-05-31 12:09:07 +0300

    -created lesson 9

---
+++ Lesson 9/hw9-2/main.js
@@ -0,0 +1,13 @@
+// #OPLI89c9G
+let arr = ['Main','Products','About us','Contacts'];
+const ul = document.createElement('ul');
+
+for(const item of arr){
+    const li = document.createElement('li');
+    li.innerText = item;
+    ul.appendChild(li);
+}
+
+document.body.appendChild(ul);
+
+
---
+++ Lesson 9/hw9-3/main.js
@@ -0,0 +1,16 @@
+// #jeBqHV525U5
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for(let course of coursesAndDurationArray) {
+    const courseDiv = document.createElement('div');
+    courseDiv.innerText = `${course.title} ${course.monthDuration}`;
+    document.body.append(courseDiv);
+}
+
